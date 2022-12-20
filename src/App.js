3d5a91a0aaa20401434171414183c5fa5bb10d35
chaw359 <g.fuccio359@gmail.com>
2022-12-20 11:32:54 +0100

    change app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Come va?
+          Welcome to React
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          I was changed on the feature branch
         </a>
       </header>
     </div>
